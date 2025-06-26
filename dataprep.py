0646693a24b8bddfb0242e1eb4d1e92c6f1b483a
soCromp <cromp@wisc.edu>
2025-06-26 12:26:30 -0500

    equal area grids without multithreading

--- dataprep.py
+++ dataprep.py
@@ -1,4 +1,3 @@
-# %%
 # nts: activate langchain_env 
 import cdsapi
 import logging
@@ -10,140 +9,177 @@ import os
 import sys
 import contextlib
 import threading
+from pyproj import Proj 
+from scipy.interpolate import RegularGridInterpolator
 
-trackspath='/home/sonia/mcms/tracker/1940-2010/era5/read_era5/out_era5_output_1940_2010.txt'
-# trackspath='/home/sonia/mcms/tracker/2010-2024/era5/out_era5/era5/mcms_era5_2010_2024_tracks.txt'
+
+###### settings
+trackspath1='/home/sonia/mcms/tracker/1940-2010/era5/out_era5/era5/mcms_era5_1940_2010_tracks.txt'
+trackspath2='/home/sonia/mcms/tracker/2010-2024/era5/out_era5/era5/mcms_era5_2010_2024_tracks.txt'
+joinyear = 2010 # overlap for the track data
 use_slp = False # whether to include slp channel
-threads = 32
-
-# %%
-# make dataframe of all tracks 
-tracks = pd.read_csv(trackspath, sep=' ', header=None, 
-        names=['year', 'month', 'day', 'hour', 'total_hrs', 'unk1', 'unk2', 'unk3', 'unk4', 'unk5', 'unk6', 
-               'z1', 'z2', 'unk7', 'tid', 'sid'])
-tracks = tracks.sort_values(by=['year', 'month', 'day', 'hour'])
-tracks['lat'] = 90-tracks['unk1'].values/100
-tracks['lon'] = tracks['unk2'].values/100
-tracks = tracks[['year', 'month', 'day', 'hour', 'tid', 'sid', 'lat', 'lon']]
-
+use_windmag = True #include wind magnitude channel
+use_winduv = False # (Not implemented) include wind u and v components channels
+threads = 1
+outpath = '/home/cyclone/train/windmag_natlantic'
+###### 
 
 regmask = xr.open_dataset('/home/cyclone/regmask_0723_anl.nc')
-reg_id = 110 # atlantic ocean
+# atlantic ocean is regmask['reg_name'].values[109] # so 110 in regmaskoc values
+reg_id = 110
 
-# %%
-box = 32 # (box/2 from center in each direction)
+####### make dataframe of all tracks 
+tracks1 = pd.read_csv(trackspath1, sep=' ', header=None, 
+        names=['year', 'month', 'day', 'hour', 'total_hrs', 'unk1', 'unk2', 'unk3', 'unk4', 'unk5', 'unk6', 
+               'z1', 'z2', 'unk7', 'tid', 'sid'])
+# storms that start before the join year (even if they continue into the join year):
+sids1 = tracks1[(tracks1['sid']==tracks1['tid']) & (tracks1['year']<joinyear)]['sid'].unique()
+tracks1 = tracks1[tracks1['sid'].isin(sids1)]
+
+tracks2 = pd.read_csv(trackspath2, sep=' ', header=None, 
+        names=['year', 'month', 'day', 'hour', 'total_hrs', 'unk1', 'unk2', 'unk3', 'unk4', 'unk5', 'unk6', 
+               'z1', 'z2', 'unk7', 'tid', 'sid'])
+# filter out storms that "start" at the beginning of the join year since they probably started before and are 
+# included in tracks1
+sids2 = tracks2[(tracks2['sid']==tracks2['tid']) & \
+        ((tracks2['year']>=joinyear) | (tracks2['month']>1) | (tracks2['day']>1) | (tracks2['hour']>0))]['sid'].unique()
+tracks2 = tracks2[tracks2['sid'].isin(sids2)]
+
+tracks = pd.concat([tracks1, tracks2], ignore_index=True)
+tracks = tracks.sort_values(by=['year', 'month', 'day', 'hour'])
+
+# conversions from the MCMS lat/lon system, as described in Jimmy's email:
+tracks['lat'] = 90-tracks['unk1'].values/100
+tracks['lon'] = tracks['unk2'].values/100
+
+tracks = tracks[['year', 'month', 'day', 'hour', 'tid', 'sid', 'lat', 'lon']]
+
+####### variables prep
+varnames = [] # list of variables that will be included in this output dataset
+varlocs = {'slp': '/home/cyclone/slp/', 'wind': '/home/cyclone/wind'} # where the source data is stored 
+varfuncs = {}
 if use_slp:
-    file_year = 1940
-    slp = xr.open_dataset('/home/cyclone/slp.1940.nc')
-    slp_next = xr.open_dataset('/home/cyclone/slp.1941.nc')
-    
+    varnames.append('slp')
+    def f_slp(ds, lats, lons): # function to run when new SLP file is loaded
+        return ds.sel(lat=lats, lon=lons, time=time)['slp']
+    varfuncs['slp'] = f_slp
+if use_windmag:
+    varnames.append('wind')
+    def f_wind(ds, lats, lons, time):
+        u = ds.sel(lat=lats, lon=lons, time=time)['u10']
+        v = ds.sel(lat=lats, lon=lons, time=time)['v10']
+        windmag = np.sqrt(u**2 + v**2)
+        return windmag
+    varfuncs['wind'] = f_wind
+varnames, varfuncs
 
-def prep_point(df, client, thread=0):
+resolution = 0.5 # resolution of data in degs
+l = 800 # (half length: l/2 km from center in each direction)
+s = 32 # box will be dimensions s by s (eg 32x32)
+x_lin = np.linspace(-l, l, s)
+y_lin = np.linspace(-l, l, s)
+x_grid, y_grid = np.meshgrid(x_lin, y_lin) # equal-spaced points from -l to l in both x and y dimensions
+
+cur_year = 1940 
+end_year = 2024
+cur_datas = {}
+next_datas = {}
+for var in varnames:
+    cur_data = xr.open_dataset(f'{varlocs[var]}/{var}.{cur_year}.nc', engine='netcdf4')
+    correct_time = cur_data['time'].values[0] + pd.to_timedelta(np.arange(cur_data.dims['time']) * 6, unit='h')
+    cur_data = cur_data.assign_coords(time=correct_time) # incase it wasn't read in as 6hrly
+    cur_datas[var] = cur_data
+    next_data = xr.open_dataset(f'{varlocs[var]}/{var}.{cur_year+1}.nc', engine='netcdf4')
+    correct_time = next_data['time'].values[0] + pd.to_timedelta(np.arange(next_data.dims['time']) * 6, unit='h')
+    next_data = next_data.assign_coords(time=correct_time) # incase it wasn't read in as 6hrly
+    next_datas[var] = next_data
+
+def prep_point(df, thread=0):
     """make one training datapoint. df contains year/../hr, lat, lon of center"""
     boxes = []
+    file_year = cur_year
     for _, frame in df.iterrows():
         year, month, day, hour = frame['year'], frame['month'], frame['day'], frame['hour']
-        lat, lon = frame['lat'], frame['lon']
-        if use_slp:
-            # get the box
-            if year==file_year:
-                slp_box = slp.sel(time=f'{year}-{month:02d}-{day:02d}T{hour:02d}:00:00',
-                                lat=slice(lat+box/4, lat-box/4), lon=slice(lon-box/4, lon+box/4))
-                                # /4, because /2 for half box and /2 for grid resolution of 0.5 degrees
-            elif year==file_year+1:
-                slp_box = slp_next.sel(time=f'{year}-{month:02d}-{day:02d}T{hour:02d}:00:00', 
-                                lat=slice(lat+box/4, lat-box/4), lon=slice(lon-box/4, lon+box/4))
-            else:
-                raise ValueError(f'Year {year} not supported, file year is {file_year}')
-            slp_box = slp_box.slp.squeeze().values
+        time = f'{year}-{month:02d}-{day:02d}T{hour:02d}:00:00'
+        if year != file_year: # go to next file year 
+            file_year += 1
+            for var in varnames:
+                cur_datas[var] = next_datas[var]
+                if file_year < end_year:
+                    next_data = xr.open_dataset(f'{varlocs[var]}/{var}.{file_year+1}.nc', engine='netcdf4')
+                    correct_time = next_data['time'].values[0] + pd.to_timedelta(np.arange(next_data.dims['time']) * 6, unit='h')
+                    next_data = next_data.assign_coords(time=correct_time) # incase it wasn't read in as 6hrly
+                    next_datas[var] = next_data
+                else:
+                    next_datas[var] = None
         
-        request = {
-            "product_type": ["reanalysis"],
-            "variable": [
-                "10m_u_component_of_wind",
-                "10m_v_component_of_wind",
-                # "sea_surface_temperature"
-            ],
-            "year": [str(year)],
-            "month": [str(month)],
-            "day": [str(day)],
-            "time": [f"{hour}:00"],
-            'format': 'netcdf',
-            "download_format": "unarchived",
-            "area": [lat+box/4, lon-box/4, lat-box/4, lon+box/4],
-            'grid': '0.5/0.5', 
-        }
-        # with suppress_output():
-        out = client.retrieve('reanalysis-era5-single-levels', request, f'temp_{str(thread)}.nc') #.download()
-
-        ds = xr.open_dataset(f'temp_{str(thread)}.nc')
-        u = ds['u10'].squeeze().values[:box, :box] # deal with rounding things
-        v = ds['v10'].squeeze().values[:box, :box]
-        magnitude = np.sqrt(u**2 + v**2)
-        boxes.append(magnitude)
+        lat_center, lon_center = frame['lat'], frame['lon']
+        # 'aeqd': https://proj.org/en/stable/operations/projections/aeqd.html
+        proj_km = Proj(proj='aeqd', lat_0=lat_center, lon_0=lon_center, units='km')
+        # Project to find lat/lon corners of the equal-area box
+        lon_grid, lat_grid = proj_km(x_grid, y_grid, inverse=True) #translate km to deg
+        lon_grid=(lon_grid+360)%360 # because these datasets have lon as 0 to 360 (lat is still -90 to 90)
+        lon_min = lon_grid.min() - resolution # +- reso because otherwise xarray will not include the edge points
+        lon_max = lon_grid.max() + resolution
+        lat_min = lat_grid.min() - resolution
+        lat_max = lat_grid.max() + resolution
+        # print(lat_max, lat_min, lon_min, lon_max)
         
-    return boxes
+        slices = []
+        for var in varnames:
+            data = varfuncs[var](cur_datas[var], slice(lat_max, lat_min), slice(lon_min, lon_max), time)
+            slices.append(data.sortby(['lat', 'lon']).squeeze().values)
+        rawbox = np.stack(slices).squeeze() # squeeze -- only works with 1 channel for now
+        # print(rawbox)
+        
+        # Build interpolator
+        interp = RegularGridInterpolator(
+            (data.lat.values, data.lon.values),
+            rawbox,
+            bounds_error=False,
+            fill_value=np.nan
+        )
 
-# %%
-sids = tracks[tracks['year'] <= 2010]['sid'].unique() # SIDS STARTING BEFORE 2010
-# sids = tracks['sid'].unique()
+        # Interpolate at new (lat, lon) pairs
+        interp_points = np.stack([lat_grid.ravel(), lon_grid.ravel()], axis=-1)
+        interp_values = interp(interp_points).reshape(s, s)
+        boxes.append(interp_values)
+        
+    datapoint = np.stack(boxes, axis=0)
+    return datapoint
+
+sids = tracks['sid'].unique()
 RADIUS=6371 # Earth radius in km
-outpath = '/home/cyclone/train/windmag_natlantic'
+
 if not os.path.exists(outpath):
     os.makedirs(outpath)
-readme = """32x32 of just wind magnitude in north atlantic ocean, 8 frames long, over [2010,2024]. Made on 18 June 2025"""
+readme = """small debugging dataset: 32x32 of just slp, 8 frames long, over [1940,1942]"""
 with open(f'{outpath}/README.txt', 'w') as f:
     f.write(readme)
 
 def worker(sids_chunk, thread_id):
-    # with suppress_output():
-    client = cdsapi.Client()
-    
     for i, sid in enumerate(sids_chunk):
         if i % 100 == 0:
             print(f'Thread {thread_id}: Processing sid {i}/{len(sids_chunk)}: {i/len(sids_chunk)*100:.2f}% complete')
-        
-        if os.path.exists(f'{outpath}/{sid}') and len(os.listdir(f'{outpath}/{sid}')) >= 8:
-            continue #already processed
         sid_df = tracks[tracks['sid'] == sid]
         start_lat = sid_df['lat'].iloc[0]
         start_lon = sid_df['lon'].iloc[0]
-        if len(sid_df) < 10:
+        if len(sid_df) < 8: # storm too short
             continue
-        elif np.abs(start_lat) > 70:
+        elif sid_df[sid_df['tid']==sid]['lat'].abs().iloc[0] > 70:
             continue # starts poleward of 70 degrees
         elif start_lat < 0 or 110 not in regmask.sel(lono=start_lon, lato=start_lat, method='nearest')['regmaskoc'].values:
             continue # only get north atlantic ocean area
-
+        
         sid_df = sid_df.sort_values(by=['tid'])
-        
-        # # check total distance traveled (sum of great circle)
-        # lat1 = np.radians(sid_df['lat'].to_numpy()[:-1])
-        # lon1 = np.radians(sid_df['lon'].to_numpy()[1])
-        # lat2 = np.radians(sid_df['lat'].to_numpy()[1:])
-        # lon2 = np.radians(sid_df['lon'].to_numpy()[1:])
-        # dlat = lat2 - lat1
-        # dlon = lon2 - lon1
-        # a = np.sin(dlat/2)**2 + np.cos(lat1) * np.cos(lat2) * np.sin(dlon/2)**2
-        # c = 2 * np.arcsin(np.sqrt(a))
-        # dist = np.sum(RADIUS * c)
-        
         sid_df = sid_df.iloc[:8]  # only take the first 8 frames for debugging
         
-        if use_slp and sid_df['year'].iloc[0] == file_year + 1: # starts in the next year
-            slp = slp_next
-            try:
-                slp_next = xr.open_dataset(f'/home/cyclone/slp.{file_year+2}.nc')
-            except:
-                slp_next = None # reaching the end of our data
-            file_year += 1 
-
-        point = prep_point(sid_df, client, thread_id)
+        point = prep_point(sid_df)
         os.makedirs(f'{outpath}/{sid}', exist_ok=True)
         for i, frame in enumerate(point):
             np.save(f'{outpath}/{sid}/{i}.npy', frame)
-
-# %%
+            
+            
 for i in range(threads):
     start = i * len(sids) // threads
     end = (i + 1) * len(sids) // threads
